609c3122f357ca2aedc1586271a6fc8a14b027bb
Makarov Andrey <winandfx@gmail.com>
2023-02-11 23:15:00 +0300

    Manage project with Poetry

--- com_interfaces/__init__.py
+++ com_interfaces/__init__.py
@@ -1,4 +1,11 @@
-__version__ = "1.0.1"
+from importlib.metadata import PackageNotFoundError, version  # type: ignore
 
 from com_interfaces.iunknown import (CArgObject, Guid, IUnknown, interface,
                                      method, structure)
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    # tried to get version of the package but it's not installed in environment
+    __version__ = "unknown"
--- setup.py
+++
@@ -1,5 +0,0 @@
-import setuptools
-
-if __name__ == "__main__":
-    # https://stackoverflow.com/a/62983901
-    setuptools.setup()
